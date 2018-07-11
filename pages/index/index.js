d29b2c99a7c421567974900a88068f5321a6b155
bai3 <2442694004@qq.com>
2018-07-11 23:51:05 +0800

    完成基本功能，但是网站是http协议，微信不能请求到数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,45 @@ const app = getApp()
 
 Page({
   data: {
-    books: [{ "id": 20, "url": "https://book.douban.com/subject/3259440/", "isbn": 9787544242516, "name": "白夜行", "author": "[日]东野圭吾", "image": "https://img3.doubanio.com/view/subject/l/public/s4610502.jpg", "publisher": " 南海出版公司", "pubdate": " 2008-9", "price": " 29.80元", "rating": 9.1 }, { "id": 4649, "url": "https://book.douban.com/subject/26927491/", "isbn": 9787550292581, "name": "白夜照相馆", "author": "新浪潮文学", "image": "https://img1.doubanio.com/view/subject/l/public/s29436947.jpg", "publisher": " 北京联合出版公司/联合读创", "pubdate": " 2017-1", "price": " 38", "rating": 6.8 }, { "id": 4805, "url": "https://book.douban.com/subject/21993104/", "isbn": 9787532760596, "name": "白夜", "author": "[俄]费奥多尔·陀思妥耶夫斯基", "image": "https://img3.doubanio.com/view/subject/l/public/s27319742.jpg", "publisher": " 上海译文出版社", "pubdate": " 2013-4", "price": " 22.00元", "rating": 8.1 }]
+    books: [],
+    keyword: '',
+    isbooks: 1
+  },
+  getList: function(e){
+    if(e.detail.value.length == 0){
+      wx.showToast({
+        title: '关键字不能为空',
+        icon: 'none',
+        duration: 2000
+      })
+      return false;
+    }
+    this.setData({
+      keyword: e.detail.value
+    });
+    var this_ = this;
+    wx.request({
+      url: 'http://www.bai3.xyz:3000/api/search',
+      method: "GET",
+      data: {
+        kw: this.data.keyword
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        if(res.data.code == 200){
+          this_.setData({
+            books: res.data.data,
+            isbooks: 2
+          })
+        }else if(res.data.code == 201){
+          this_.setData({
+            books: [],
+            isbooks: 3
+          })
+        }
+      },
+    })
   },
 })
